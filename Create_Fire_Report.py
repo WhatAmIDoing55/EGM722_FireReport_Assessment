4ed541b536e5c3a24d37d38766543ef3b443502e
WhatAmIDoing55 <101790326+WhatAmIDoing55@users.noreply.github.com>
2023-04-15 23:22:43 +0300

    New Environment
    Created a new environment file to fix 'ModuleNotFoundError'. Still not working so left a message on the discussion board.

--- Create_Fire_Report.py
+++ Create_Fire_Report.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# Libraries
+import requests
+import pandas as pd
+
+# Fire Data
+# SUOMI VIIRS - Last 7 days
+snpp_url = 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_Global_7d.csv'
+
+snpp_df = pd.read_csv(snpp_url)
