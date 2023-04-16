084f54d66b62a8e409cd3109b56840c592068a03
WhatAmIDoing55 <101790326+WhatAmIDoing55@users.noreply.github.com>
2023-04-16 13:52:43 +0300

    Create Jupyter NoteBook
    Created Notebook to test each part of the script during development.

--- Create_Fire_Report.py
+++ Create_Fire_Report.py
@@ -1,10 +1,10 @@
 print("Starting Script........")
 
-# Libraries
+""" Libraries """
 import requests
 import pandas as pd
 
-# Fire Data
+""" Fire Data """
 # SUOMI VIIRS - Last 7 days
 snpp_url = 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_Global_7d.csv'
 
