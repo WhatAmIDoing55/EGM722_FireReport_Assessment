4310dab584576fd3c7864b7eb5e90007d7c6a929
WhatAmIDoing55 <101790326+WhatAmIDoing55@users.noreply.github.com>
2023-05-03 22:38:47 +0300

    Remove fires within geofence
    An issue with the previous map and graph was fire detection coming from industrial areas which are not 'real' fires. Such as power plants and refinaries etc.
    Solution:
    - Create a GeoFence which is a polygon shapefile drawn around the feature that causes false detections.
    Geopandas used to select fires within these and remove them.
    - Script re-ordered so that this step happens before the graph.
    - Pandas df can read gdf so no issue with graph.

--- Create_Fire_Report.py
+++ Create_Fire_Report.py
@@ -10,52 +10,72 @@ import datetime
 # import folium  #saved for future use
 # import fpdf  #saved for future use
 
+"""  Static GIS Data  """
+aoi = gpd.read_file(os.path.abspath('Data/UTM36/AOI.shp'))
+coastline = gpd.read_file(os.path.abspath('Data/UTM36/Island.shp'))
+towns = gpd.read_file(os.path.abspath('Data/UTM36/Cyprus_Towns.shp'))
+geofence = gpd.read_file(os.path.abspath('Data/UTM36/geofence.shp'))
+
 """  Fire Data  """
 # SUOMI VIIRS - Last 7 days Europe
 snpp_url = 'https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_Europe_7d.csv'
 
-snpp_df = pd.read_csv(snpp_url)  # create the snpp_df dataframe from the url
+#  Create the snpp_df dataframe from the url
+snpp_df = pd.read_csv(snpp_url)
+
+#  Create a new dataframe with values less than or more than lat/lon for cyprus
 snpp_df = snpp_df[(snpp_df['longitude'] >= 32.0) & (snpp_df['longitude'] <= 35.0)]
 snpp_df = snpp_df[(snpp_df['latitude'] >= 34.5) & (snpp_df['latitude'] <= 36.0)]
 
 print(snpp_df.head())  # Print the head of the dataset to show it has been downloaded correctly
 
+#   Create a new geodataframe for active fires
+all_Fires = gpd.GeoDataFrame(snpp_df[['acq_date', 'bright_ti4']],
+                             # use the csv data, but only the name/website columns
+                             geometry=gpd.points_from_xy(snpp_df['longitude'], snpp_df['latitude']),
+                             # set the geometry using points_from_xy
+                             crs='epsg:4326')
+print(all_Fires.head())  # print the head of the new activeFires geopandas dataframe to show it worked correctly
+
+#  Reproject the Fire data from WGS 84 to UTM 36 North
+all_Fires_UTM = all_Fires.to_crs('epsg:32636')
+
+#  Save activeFires_UTM as a shapefile
+all_Fires_UTM.to_file(driver='ESRI Shapefile', filename='Data/UTM36/AllFiresUTM.shp')
+
+# All Fires UTM Shapefile
+fires = gpd.read_file(os.path.abspath('Data/UTM36/AllFiresUTM.shp'))
+
+
+"""  Remove Fire Detection from Industrial Locations  """
+#  Repeated fire detections at refinery cause issues with map and graph because user will think these are 'real' fires.
+#  geofence is a shapefile which will be used to remove fires which intersect with this feature.
+#  Find fires within 'geofence'
+industrial_detections = gpd.sjoin(fires, geofence, predicate='within')
+#  Remove fires within geofence
+active_Fires = fires[~fires.index.isin(industrial_detections.index)]
+#  Save as shapefile and rename value for map
+active_Fires.to_file('Data/UTM36/ActiveFires.shp')
+map_fires = gpd.read_file(os.path.abspath('Data/UTM36/ActiveFires.shp'))
+
 """  FIRE GRAPH  """
 #  Create a graph to show number of fires per day
-dates = snpp_df['acq_date'].value_counts().sort_index()  # Sort by date
+#  Graph is placed here so that it is plotted after removing fires inside geofence
+df1 = pd.DataFrame(active_Fires)
+dates = df1['acq_date'].value_counts().sort_index()  # Sort by date
 plt.plot(dates, color='red', marker='.', label='Fires')
 plt.title('Cyprus: Fire detected within the last 7 days', family='Arial', fontsize='14')
 plt.xlabel('Date')
 plt.ylabel('Number of Detections')
 plt.tick_params(axis='x', rotation=55)
-plt.savefig('FireGraph.png', bbox_inches='tight')
-
-#   Create a new geodataframe for active fires
-activeFires = gpd.GeoDataFrame(snpp_df[['acq_date', 'bright_ti4']],
-                               # use the csv data, but only the name/website columns
-                               geometry=gpd.points_from_xy(snpp_df['longitude'], snpp_df['latitude']),
-                               # set the geometry using points_from_xy
-                               crs='epsg:4326')
-print(activeFires.head())  # print the head of the new activeFires geopandas dataframe to show it worked correctly
-
-#  Reproject the Fire data from WGS 84 to UTM 36 North
-activeFires_UTM = activeFires.to_crs('epsg:32636')
-
-#  Save activeFires_UTM as a shapefile
-activeFires_UTM.to_file(driver='ESRI Shapefile', filename='Data/UTM36/ActiveFiresUTM.shp')
-
-"""  GIS DATA  """
-aoi = gpd.read_file(os.path.abspath('Data/UTM36/AOI.shp'))
-coastline = gpd.read_file(os.path.abspath('Data/UTM36/Island.shp'))
-fires = gpd.read_file(os.path.abspath('Data/UTM36/ActiveFiresUTM.shp'))
-towns = gpd.read_file(os.path.abspath('Data/UTM36/Cyprus_Towns.shp'))
+plt.savefig('FireGraph.png', bbox_inches='tight', dpi=100)
 
 """  Create Map  """
-
-
 #  Function to create 50km alternating scale bar with label at each end
 #  adapted this question: https://stackoverflow.com/q/32333870
 #  answered by SO user Siyh: https://stackoverflow.com/a/35705477
+
+
 def scale_bar(ax, location=(0.5, 0.05)):
     x0, x1, y0, y1 = ax.get_extent()
     sbx = x0 + (x1 - x0) * location[0]
@@ -71,12 +91,13 @@ def scale_bar(ax, location=(0.5, 0.05)):
     ax.text(sbx, sby - 6000, '50 km', transform=ax.projection, fontsize=8)
     ax.text(sbx - 55000, sby - 6000, '0 km', transform=ax.projection, fontsize=8)
 
+
 #  Function to dd the current date / time as a text box to the bottom right corner of the map
 def current_time():
     now = datetime.datetime.now()
     date_str = now.strftime("Fire data downloaded on\n %Y-%m-%d at %H:%M:%S")
     ax.text(0.75, 0.05, date_str, transform=ax.transAxes, ha='left', va='bottom', wrap=True,
-            bbox=dict(boxstyle="round", ec=('black'), fc=('white')))
+            bbox=dict(boxstyle="round", ec='black', fc='white'))
 
 # Create figure
 myFig = plt.figure(figsize=(10, 10), facecolor='0.7')
@@ -117,14 +138,14 @@ ax.plot(towns.geometry.x, towns.geometry.y, '.', color='black', ms=4, transform=
 for ind, row in towns.iterrows():  # towns.iterrows() returns the index and row
     x, y = row.geometry.x, row.geometry.y  # get the x,y location for each town
     ax.text(x + 1000, y + 1000, row['Name'].title(), fontsize=8,
-            transform=myCRS)  # use plt.text to place a label at x,y
+            transform=myCRS)
 
 # Add Fire Data to Map
-ax.plot(fires.geometry.x, fires.geometry.y, 's', color='red', ms=4, transform=myCRS)
+ax.plot(map_fires.geometry.x, map_fires.geometry.y, 's', color='red', ms=4, transform=myCRS)
 
 #  Label Fires by date
-for ind, row in fires.iterrows():  # towns.iterrows() returns the index and row
-    x, y = row.geometry.x, row.geometry.y  # get the x,y location for each town
+for ind, row in map_fires.iterrows():
+    x, y = row.geometry.x, row.geometry.y
     ax.text(x + 1000, y + 1000, row['acq_date'].title(), fontsize=8, transform=myCRS)
 
 #  Add Graticule and labels
@@ -149,6 +170,7 @@ current_time()
 
 #  Save the map as a png
 myFig.savefig('LatestFireMap.png', bbox_inches='tight', dpi=300)
+
 
 """Create PDF Report"""
 
@@ -187,12 +209,13 @@ pdf.cell(60, 10, title, 1, 1, 'C')
 # Add an information paragraph below the Fire Map
 pdf.set_font('Arial', '', 10)
 pdf.set_xy(10,183)
-pdf.multi_cell(w = 200, h = 8, txt ="This map shows active fires on the island of cyprus within the last 7 days.", border = 0,
-                align = 'L', fill = False)
+pdf.multi_cell(w = 200, h = 8, txt ="This map shows active fires on the island of cyprus within the last 7 days.", 
+                border = 0, align = 'L', fill = False)
 
 pdf.set_xy(10,250)
-pdf.multi_cell(w = 200, h = 8, txt ="This report has been auto-generated using SNPP VIIRS data, which has been downloaded from the NASA FIRMS Website. For more information please click on the link below: ", border = 0,
-                align = 'L', fill = False)
+pdf.multi_cell(w = 200, h = 8, txt ="This report has been auto-generated using SNPP VIIRS data, which has been 
+                downloaded from the NASA FIRMS Website. For more information please click on the link below: ", 
+                border = 0, align = 'L', fill = False)
 
 pdf.set_font('Arial', 'U', 10)
 pdf.set_text_color(r = 0, g = 0, b = 254)
